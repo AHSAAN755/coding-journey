490c2f05f55f22a954a8fe5ff1bbc7a468020501
ahsaan755 <ahsaanayaan755@gmail.com>
2025-11-08 18:18:46 +0530

    Create ispalindrome.c

---
+++ c/ispalindrome.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int n,n1,r,re=0,sum=0;
+	printf("enter a positive number:");
+	scanf("%d",&n);
+	n1=n;
+	while(n>10)
+	{
+		r = n%10;
+		sum = sum + r;
+		re = re * 10 + r;
+		n /= 10;
+	}
+	printf("sum of digits is %d \n",sum);
+	if(n1==re)
+	{
+		printf("%d is a palindrome",n1);
+	}else{
+		printf("%d is not a palindrome",n1);		
+	}
+}
