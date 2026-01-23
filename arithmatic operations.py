95884acccd447598eadca3a95ffa2eb3426670fe
AHSAAN755 <ahsaanayaan755@gmail.com>
2026-01-23 14:53:36 +0530

    Add files via upload

---
+++ arithmatic operations.py
@@ -0,0 +1,9 @@
+#program for arithmatic operations
+print("Enter the first number")
+a=int(input())
+print("Enter the second number")
+b=int(input())
+print("Addition is:",a+b)
+print("Subtraction is:",a-b)
+print("Multiplication is:",a*b)
+print("Divition is:",a/b)
